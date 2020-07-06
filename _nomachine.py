cf967af8c4d9d6910164a8f62822f937dd1b0725
James Murphy <james@intfxdx.com>
2020-07-06 10:22:24 -0500

    no machine grammar

---
+++ _nomachine.py
@@ -0,0 +1,20 @@
+from dragonfly import *
+
+
+class NomachineRule(MappingRule):
+    mapping = {
+        'toggle fullscreen': Key('ca-f'),
+        '[toggle] grab keyboard [input]': Key('ca-k'),
+        'no machine (menu|settings)': Key('ca-0'),
+    }
+
+
+nomachine_grammar = Grammar('nomachine')
+nomachine_grammar.add_rule(NomachineRule())
+nomachine_grammar.load()
+
+
+def unload():
+    global nomachine_grammar
+    if nomachine_grammar: nomachine_grammar.unload()
+    nomachine_grammar = None
