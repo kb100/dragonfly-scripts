72627632c64bdbc44159c529da9268a9650f0bcd
James Murphy <james@intfxdx.com>
2020-07-07 19:06:58 -0500

    move grammar switcher to separate file

--- _gvim.py
+++ _gvim.py
@@ -2,6 +2,7 @@
 
 from lib.common import executeSelect, LetterRef, LetterSequenceRef, singleCharacterKeyMap, EmptyAction
 from lib.format import FormatRule
+from lib.grammar_switcher import GrammarSwitcher
 from python_language import PythonRule
 
 gvim_exec_context = AppContext(executable="gvim")
@@ -16,42 +17,6 @@ insert_mode_grammar = Grammar("InsertMode", context=gvim_context)
 pycharm_grammar = Grammar('pycharm global', context=gvim_context)
 
 EXPORT_GRAMMARS = [pycharm_grammar, normal_mode_grammar, visual_mode_grammar, insert_mode_grammar, ex_mode_grammar]
-
-
-class GrammarSwitcher:
-    def __init__(self, grammars=None):
-        self.grammars = grammars if grammars is not None else []
-
-    def switch_to(self, grammar):
-        for other in self.grammars:
-            if other is grammar:
-                grammar.enable()
-            else:
-                other.disable()
-
-    def add_grammar(self, grammar):
-        self.grammars.append(grammar)
-
-    def switch_to_action(self, grammar):
-        return Function(self.switch_to, grammar=grammar)
-
-    def switches_to(self, grammar):
-        assert [g for g in self.grammars if g is grammar]
-
-        def class_modifier(cls):
-            assert hasattr(cls, 'value')
-            assert callable(cls.value)
-            old_value = cls.value
-
-            def value(this, node):
-                v = old_value(this, node)
-                return v + self.switch_to_action(grammar)
-
-            cls.value = value
-            return cls
-
-        return class_modifier
-
 
 grammar_switcher = GrammarSwitcher([normal_mode_grammar, visual_mode_grammar, insert_mode_grammar, ex_mode_grammar])
 
---
+++ lib/grammar_switcher.py
@@ -0,0 +1,33 @@
+class GrammarSwitcher:
+    def __init__(self, grammars=None):
+        self.grammars = grammars if grammars is not None else []
+
+    def switch_to(self, grammar):
+        for other in self.grammars:
+            if other is grammar:
+                grammar.enable()
+            else:
+                other.disable()
+
+    def add_grammar(self, grammar):
+        self.grammars.append(grammar)
+
+    def switch_to_action(self, grammar):
+        return Function(self.switch_to, grammar=grammar)
+
+    def switches_to(self, grammar):
+        assert [g for g in self.grammars if g is grammar]
+
+        def class_modifier(cls):
+            assert hasattr(cls, 'value')
+            assert callable(cls.value)
+            old_value = cls.value
+
+            def value(this, node):
+                v = old_value(this, node)
+                return v + self.switch_to_action(grammar)
+
+            cls.value = value
+            return cls
+
+        return class_modifier
