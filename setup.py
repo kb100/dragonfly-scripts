908a9aab18795ea45ee800a12faf4b81a51d2919
James Murphy <james@intfxdx.com>
2020-07-12 12:01:07 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dragonfly-scripts',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/kb100/dragonfly-scripts/',
+    author='James Murphy',
+    description='My personal dragonfly scripts for use with Dragon Naturally Speaking and Natlink',
+)
