3df5394fc088a2e8fcfb384391b2b2a4dfa14d83
James Murphy <james@intfxdx.com>
2020-07-12 16:00:33 -0500

    allow nato alphabet in format functions, add format tests

--- lib/format.py
+++ lib/format.py
@@ -1,75 +1,111 @@
-# Format: some_words
 from dragonfly import Text, Function, MappingRule, Dictation
 
+from lib.common import lowercase_key_map
 
-def format_score(dictation):  # Function name must start with "format_".
-    """ (snake|score) <dictation> """  # Docstring defining spoken-form.
-    text = str(dictation)  # Get written-form of dictated text.
-    return "_".join(text.split(" "))  # Put underscores between words.
 
-# __some_words
+def replace_lowercase_keys(words):
+    return [lowercase_key_map.get(word, word) for word in words]
+
+
+def words_from_dictation(dictation):
+    text = str(dictation)
+    text = text.lower()
+    text = text.replace('-', ' ')
+    words = text.split(' ')
+    words = replace_lowercase_keys(words)
+    return words
+
+
+def squash_single_letter_words_together(words):
+    new_words = []
+    curr = []
+    for word in words:
+        if len(word) == 1:
+            curr.append(word)
+        else:
+            if curr:
+                new_words.append(''.join(curr))
+                curr = []
+            new_words.append(word)
+    if curr:
+        new_words.append(''.join(curr))
+    return new_words
+
+
+# Format: some_words
+def format_score(dictation):
+    """ (snake|score) <dictation> """
+    words = words_from_dictation(dictation)
+    words = squash_single_letter_words_together(words)
+    return "_".join(words)
+
+
+# Format: __some_words
 def format_double_score(dictation):
     """ (double score|double under|dunder) <dictation> """
-    text = str(dictation)
-    return "__" + "_".join(text.split(" "))
+    words = words_from_dictation(dictation)
+    words = squash_single_letter_words_together(words)
+    return "__" + "_".join(words)
 
 
 # Format: some_words()
 def format_under_function(dictation):
     """ under func <dictation> """
-    text = str(dictation)
-    return "_".join(text.split(" ")) + "()"
+    words = words_from_dictation(dictation)
+    words = squash_single_letter_words_together(words)
+    return "_".join(words) + "()"
 
 
 # Format: SomeWords
 def format_title(dictation):
     """ title <dictation> """
-    text = str(dictation)
-    words = [word.capitalize() for word in text.split(" ")]
+    words = words_from_dictation(dictation)
+    words = [word.capitalize() for word in words]
     return "".join(words)
 
 
 # Format: somewords
 def format_one_word(dictation):
     """ [all] one word <dictation> """
-    text = str(dictation)
-    return "".join(text.split(" "))
+    words = words_from_dictation(dictation)
+    return "".join(words)
 
 
 # Format: SOMEWORDS
 def format_upper_one_word(dictation):
     """ one word upper <dictation> """
-    text = str(dictation)
-    words = [word.upper() for word in text.split(" ")]
+    words = words_from_dictation(dictation)
+    words = [word.upper() for word in words]
     return "".join(words)
 
 
 # Format: SOME_WORDS
 def format_upper_score(dictation):
     """ upper score <dictation> """
-    text = str(dictation)
-    words = [word.upper() for word in text.split(" ")]
+    words = words_from_dictation(dictation)
+    words = squash_single_letter_words_together(words)
+    words = [word.upper() for word in words]
     return "_".join(words)
 
 
 # Format: someWords
 def format_camel(dictation):
     """ camel <dictation> """
-    text = str(dictation)
-    words = text.split(" ")
+    words = words_from_dictation(dictation)
     return words[0] + "".join(w.capitalize() for w in words[1:])
+
 
 # Format: some words
 def format_say(dictation):
     """ say <dictation> """
-    text = str(dictation)
-    return text
+    words = words_from_dictation(dictation)
+    return ' '.join(words)
 
 
 def FormatAction(function):
-    def wrap_function(function):
+    def wrap_function(f):
         def _function(dictation):
-            formatted_text = function(dictation)
+            formatted_text = f(dictation)
             Text(formatted_text).execute()
 
         return Function(_function)
---
+++ test/test_format.py
@@ -0,0 +1,86 @@
+import pytest
+
+from lib.format import *
+
+
+@pytest.mark.parametrize('words,expected', [
+    (['alpha', 'bravo'], ['a', 'b']),
+    (['test', 'alpha', 'bravo'], ['test', 'a', 'b']),
+])
+def test_replace_lowercase_keys(words, expected):
+    actual = replace_lowercase_keys(words)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('words,expected', [
+    (['a'], ['a']),
+    (['test'], ['test']),
+    (['a', 'b', 'c', 'variable'], ['abc', 'variable']),
+    (['variable', 'a', 'b', 'c'], ['variable', 'abc']),
+])
+def test_squash_single_letter_words(words, expected):
+    actual = squash_single_letter_words_together(words)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('dictation,expected', [
+    ('some words', 'some_words'),
+    ('oneword', 'oneword'),
+    ('one two three', 'one_two_three'),
+    ('non-negotiable', 'non_negotiable'),
+    ('alpha variable', 'a_variable'),
+    ('alpha bravo charlie variable', 'abc_variable'),
+    ('type tango', 'type_t'),
+    ('Proper Noun', 'proper_noun'),
+])
+def test_format_score(dictation, expected):
+    actual = format_score(dictation)
+    assert actual == expected
+
+
+def test_format_double_score():
+    actual = format_double_score('some words')
+    expected = '__some_words'
+    assert actual == expected
+
+
+def test_format_under_func():
+    actual = format_under_function('some words')
+    expected = 'some_words()'
+    assert actual == expected
+
+
+def test_format_title():
+    actual = format_title('some words')
+    expected = 'SomeWords'
+    assert actual == expected
+
+
+def test_format_one_word():
+    actual = format_one_word('some words')
+    expected = 'somewords'
+    assert actual == expected
+
+
+def test_format_upper_one_word():
+    actual = format_upper_one_word('some words')
+    expected = 'SOMEWORDS'
+    assert actual == expected
+
+
+def test_format_upper_score():
+    actual = format_upper_score('some words')
+    expected = 'SOME_WORDS'
+    assert actual == expected
+
+
+def test_format_camel():
+    actual = format_camel('some words')
+    expected = 'someWords'
+    assert actual == expected
+
+
+def test_format_say():
+    actual = format_say('some words')
+    expected = 'some words'
+    assert actual == expected
