48cf03013b45f92f797989bef716368b4bf9b13a
James Murphy <james@intfxdx.com>
2020-07-16 19:14:32 -0500

    more visual studio commands

--- _visual_studio.py
+++ _visual_studio.py
@@ -2,12 +2,45 @@
 
 from cpp_language import CPlusPlusRule
 from gvim import InsertModeRule, VimGrammarSwitcher, NormalModeRule, VisualModeRule, ExModeRule, VimMode
-from lib.common import LetterRef
+from lib.common import LetterRef, execute_select
+
+
+def toggle_vim():
+    return Key('cs-f12/10')
+
+
+def focus_search():
+    return Key('c-q/10')
+
+
+def visual_studio_action(s):
+    return toggle_vim() + focus_search() + Text(s) + Pause('10') + Key('enter/10') + toggle_vim()
 
 
 class VisualStudioGlobalRule(MappingRule):
     mapping = {
-        'foo': Text('foobar'),
+        'save all': Key('cs-s'),
+        "go to definition": Key("f12"),
+        '(show|find) [all] (usages|references)': Key("a-f7"),
+        'peek definition': Key('a-f12'),
+        '(show params|param info)': Key('cs-space'),
+        '(show type|quick info)': visual_studio_action('Quick Info'),
+        'show (type|class) (view|hierarchy)': Key('cs-c'),
+        'show call hierarchy': Key('ca-k'),
+        '(format|reformat) [document]': visual_studio_action('Reformat Document'),
+        '(format|reformat) selection': visual_studio_action('Reformat Selection'),
+        '[toggle] [line] comment': visual_studio_action('Toggle Line Comment'),
+        '[toggle] block comment': visual_studio_action('Toggle Block Comment'),
+        'suggest': Key('c-dot'),
+        '(cell|select) [<n>]': Function(execute_select, offset=1),
+        'show error': visual_studio_action('Quick Info'),
+        'rename': visual_studio_action('Rename'),
+        'extract variable': Key('c-dot'),
+        'close tab': Key('c-f4'),
+        'git commit': Key('c-0,g'),
+        'next diff': Key('c-f8'),
+        'previous diff': Key('cs-f8'),
+
     }
     extras = [
         IntegerRef('n', 1, 10, default=1),
