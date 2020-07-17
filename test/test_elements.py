a193acfd369c3e1365f7ec965dc2066d2195e2bb
James Murphy <james@intfxdx.com>
2020-07-16 20:03:38 -0500

    more tests for exclusion

--- test/test_elements.py
+++ test/test_elements.py
@@ -6,13 +6,14 @@ from lib.elements import Exclusion, PhrasesExclusion
 
 def test_exclusion(engine):
     choice = Choice('choice', choices={'a': 1, 'b': 2})
-    repetition = Repetition(choice, min=2, max=3)
+    repetition = Repetition(choice, min=2, max=5)
     to_exclude = Literal('a a')
     exclusion = Exclusion(element=repetition, exclusion=to_exclude)
 
     tester = ElementTester(exclusion, engine)
     assert tester.recognize('a a') is RecognitionFailure
     assert tester.recognize('c') is RecognitionFailure
+    assert tester.recognize('b a a b') == [2, 1, 1, 2]
     assert tester.recognize('a b') == [1, 2]
     assert tester.recognize('b a') == [2, 1]
     assert tester.recognize('b b') == [2, 2]
@@ -20,13 +21,15 @@ def test_exclusion(engine):
 
 def test_exclusion_by_function(engine):
     choice = Choice('choice', choices={'a': 1, 'b': 2})
-    repetition = Repetition(choice, min=2, max=3)
+    repetition = Repetition(choice, min=2, max=5)
     exclude_if = lambda words: 'a a' in ' '.join(words)
     exclusion = Exclusion(element=repetition, exclude_if_func=exclude_if)
 
     tester = ElementTester(exclusion, engine)
     assert tester.recognize('a a') is RecognitionFailure
     assert tester.recognize('c') is RecognitionFailure
+    assert tester.recognize('b a a b') is RecognitionFailure
+    assert tester.recognize('b b a b') == [2, 2, 1, 2]
     assert tester.recognize('a b') == [1, 2]
     assert tester.recognize('b a') == [2, 1]
     assert tester.recognize('b b') == [2, 2]
@@ -34,7 +37,7 @@ def test_exclusion_by_function(engine):
 
 def test_phrases_exclusion(engine):
     choice = Choice('choice', choices={'a': 1, 'b': 2})
-    repetition = Repetition(choice, min=2, max=3)
+    repetition = Repetition(choice, min=2, max=5)
     phrases = ['a a', 'b b']
     exclusion = PhrasesExclusion(element=repetition, phrases_to_exclude=phrases)
 
@@ -43,4 +46,6 @@ def test_phrases_exclusion(engine):
     assert tester.recognize('c') is RecognitionFailure
     assert tester.recognize('a b') == [1, 2]
     assert tester.recognize('b a') == [2, 1]
+    assert tester.recognize('b a a b') is RecognitionFailure
+    assert tester.recognize('b a b a') == [2, 1, 2, 1]
     assert tester.recognize('b b') is RecognitionFailure
