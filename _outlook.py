222f00649fd8bef8208d4cd6d3a205050b174cc6
James Murphy <james@intfxdx.com>
2020-07-18 15:29:04 -0500

    outlook first draft complete

--- _outlook.py
+++ _outlook.py
@@ -1,4 +1,5 @@
 from dragonfly import *
+
 from lib.common import LetterSequenceRef, release
 
 rules = MappingRule(
@@ -9,74 +10,60 @@ rules = MappingRule(
         "say <text>": release + Text('%(text)s'),
         "slap": Key("enter"),
         "new (email|message)": Key("cs-m"),
+        "open [email|message]": Key("c-o"),
         "reply [email|message]": Key("c-r"),
         "reply all [email|message]": Key("cs-r"),
+        "forward [email|message]": Key("c-f"),
+        "print [email|message]": Key("c-p"),
+        "send [email|message] now": Key("a-s"),
+        "save [email|message] as draft": Key("c-s"),
         "select all": Key("c-a"),
+        "[insert|start] bullet list": Key("cs-l"),
+        "insert hyperlink": Key("c-k"),
+        "align left": Key("c-l"),
+        "align center": Key("c-e"),
+        "align right": Key("c-r"),
+        "spell check": Key("f7"),
+        "subject": Key("a-j"),
         "send receive [all]": Key("f9"),
         "del": Key("c-d"),
         "mark [email|message] as read": Key("c-q"),
         "mark [email|message] as unread": Key("c-u"),
         "flag [email|message] [for] follow up": Key("cs-g"),
-        "search": Key("c-e"),
-        "back": Key("a-left"),
-        "forward": Key("a-right"),
-        "toggle left [pane|panel]": Key("cs-d"),
-        "go left [pane|panel]": Key("cs-d:2"),
+        "new task": Key("cs-k"),
+        "inbox": Key("cs-l"),
+        "outbox": Key("cs-o"),
+        "previous [email|message]": Key("c-comma"),
+        "next [email|message]": Key("c-period"),
+        "previous appointment": Key("cs-comma"),
+        "next appointment": Key("cs-period"),
+        "(search|find)": Key("c-e"),
+        "(new|create) appointment": Key("cs-a"),
+        "(new|create) meeting request": Key("cs-q"),
+        "go to": Key("c-g"),
+        "mail view": Key("c-1"),
+        "calendar view": Key("c-2"),
+        "contacts view": Key("c-3"),
+        "(task|tasks) view": Key("c-4"),
+        "(note|notes) view": Key("c-5"),
+        "(folder|folders) view": Key("c-6"),
+        "(new|create) folder": Key("cs-e"),
         "next": Key("tab"),
         "previous": Key("s-tab"),
         "[<n>] increase text size": Key("c-rangle:%(n)d"),
         "[<n>] decrease text size": Key("c-langle:%(n)d"),
         "[<n>] scroll up": Key("pageup:%(n)d"),
         "[<n>] scroll down": Key("pagedown:%(n)d"),
-        "[<n>] previous day": Key("s-pageup:%(n)d"),
-        "[<n>] next day": Key("s-pagedown:%(n)d"),
-        "toggle right (pane|panel)": Key("c-."),
-        "channel info": Key("cs-i"),
-        "[show] (mentions|reactions)": Key("cs-m"),
-        "(people|directory)": Key("cs-e"),
-        "saved items": Key("cs-s"),
-        "toggle full screen": Key("cs-f"),
-        "unread messages": Key("c-j"),
-        "select [text] [to] beginning of [current] line": Key("s-up"),
-        "select [text] [to] end of [current] line": Key("s-down"),
-        "new line": Key("s-enter"),
-        "react to last [message]": Key("cs-backslash"),
-        "auto complete username": Key("at"),
-        "auto complete channel": Key("hash"),
-        "auto complete emoji": Key("colon"),
         "bold [text|that]": Key("c-b"),
         "(italic|italicize) [text|that]": Key("c-i"),
         "(strike|strikethrough) [text|that]": Key("cs-x"),
-        "format [text|that] as code": Key("cs-c"),
-        "format [selection] [as] bullet list": Key("cs-8"),
-        "format [selection] [as] numbered list": Key("cs-7"),
-        "format [selection] [as] quote": Key("cs-9"),
-        "format selection as code": Key("cas-c"),
-        "Mark [all] messages in [current] channel as read": Key("escape"),
-        "Mark all messages as read": Key("s-escape"),
-        "Mark unread": Key("u"),
-        "previous (channel|message|DM)": Key("a-up"),
-        "next (channel|message|DM)": Key("a-down"),
-        "previous unread (channel|message|DM)": Key("as-up"),
-        "next unread [channel|message|DM]": Key("as-down"),
-        "add [a] file": Key("c-u"),
-        "create [a] snippet": Key("cs-enter"),
-        "view [all] downloads": Key("cs-j"),
-        "edit [message]": Key("e"),
-        "delete [message]": Key("delete"),
-        "(add reaction|react)": Key("r"),
-        "open thread": Key("t"),
-        "toggle pin": Key("p"),
-        "share [message]": Key("s"),
-        "toggle (star|favorite)": Key(""),
-        "Mark as unread from (here|this message)": Key("u"),
-        "remind me about this [message]": Key("m"),
+        "underline": Key("c-u"),
 
     },
     extras=[
         LetterSequenceRef('letter_sequence'),
         Dictation("text"),
-        ShortIntegerRef('n', 1, 101,default=1)
+        ShortIntegerRef('n', 1, 101, default=1)
     ],
 )
 context = AppContext(executable="outlook")
@@ -85,7 +72,9 @@ outlook_grammar.add_rule(rules)
 outlook_grammar.load()
 
 EXPORT_GRAMMARS = [outlook_grammar]
+
+
 def unload():
     global outlook_grammar
     if outlook_grammar: outlook_grammar.unload()
-    outlook_grammar= None
+    outlook_grammar = None
