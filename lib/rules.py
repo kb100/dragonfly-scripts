0f1d9ec8b51e9460b5958876215184ec8ee9f37c
James Murphy <james@intfxdx.com>
2020-07-18 19:10:18 -0500

    give repeat action rules an anonymous id count

--- lib/rules.py
+++ lib/rules.py
@@ -4,16 +4,22 @@ from lib.actions import EmptyAction
 
 
 class RepeatActionRule(CompoundRule):
-    def __init__(self, element, name=None):
+    _repeat_action_rule_count = 0
+
+    def __init__(self, element, name=None, exported=None):
         """
         :param element: element whose value is an ActionBase
         :param name:
         """
+        if name is None:
+            name = self.__class__.__name__ + str(self._repeat_action_rule_count)
+            RepeatActionRule._repeat_action_rule_count += 1
         spec = "<sequence> [<n> times]"
         extras = [Repetition(element, min=1, max=7, name="sequence"),
                   IntegerRef("n", 1, 100), ]
         self.defaults = {'n': 1}
-        super(RepeatActionRule, self).__init__(name, spec, extras)
+
+        super(RepeatActionRule, self).__init__(name=name, spec=spec, extras=extras, exported=exported)
 
     def value(self, node):
         seq = node.get_child_by_name('sequence', shallow=True).value()
